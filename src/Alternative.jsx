b02cd70d40efcb022084bedda84632cd5094d437
York Yu <n11453559@gmail.com>
2025-09-09 22:02:26 +1000

    refactor: simplify index management in nextPerson and prevPerson functions

--- src/Alternative.jsx
+++ src/Alternative.jsx
@@ -9,16 +9,26 @@ const App = () => {
   const [index, setIndex] = useState(0)
   const { name, job, image, text } = people[index]
 
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0
+    }
+    if (number < 0) {
+      return people.length - 1
+    }
+    return number
+  }
+
   const nextPerson = () => {
     setIndex((currentIndex) => {
-      let newIndex = (currentIndex + 1) % people.length
-      return newIndex
+      let newIndex = currentIndex + 1
+      return checkNumber(newIndex)
     })
   }
   const prevPerson = () => {
     setIndex((currentIndex) => {
-      let newIndex = (currentIndex - 1 + people.length) % people.length
-      return newIndex
+      let newIndex = currentIndex - 1
+      return checkNumber(newIndex)
     })
   }
 
@@ -27,8 +37,7 @@ const App = () => {
     if (randomNumber === index) {
       randomNumber = index + 1
     }
-    const newIndex = randomNumber % people.length
-    setIndex(newIndex)
+    setIndex(checkNumber(randomNumber))
   }
 
   return (
